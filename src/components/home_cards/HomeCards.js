7fd0f0e3af954d6bab50fa0a741635e5604b7083
Eric Schwartz <erics273@gmail.com>
2023-10-17 23:13:06 -0500

    tweak button color

--- src/components/home_cards/HomeCards.js
+++ src/components/home_cards/HomeCards.js
@@ -13,12 +13,12 @@ function HomeCards(props) {
                         <Card.Img variant="top" src="https://media.cntraveler.com/photos/6488ae641b82d36ab296a8be/master/w_2580%2Cc_limit/Badlands-National-Park-michael-TFBum6SWKi8-unsplash.jpg" />
                         <Card.Body>
                             <Card.Text>
-                                Visit our National Parks search page to find the park that is just right for them
+                                Visit our National Parks search page to find the park that is just right for you!
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="d-flex justify-content-center">
                             <LinkContainer to="/nationalparks">
-                                <Button variant="primary">
+                                <Button variant="secondary">
                                     National Parks Search
                                 </Button>
                             </LinkContainer>
@@ -30,12 +30,12 @@ function HomeCards(props) {
                         <Card.Img variant="top" src="https://media.cntraveler.com/photos/6488ae99c39a02ba647f7b4b/master/w_2580%2Cc_limit/Great-Smoky-Mountains-National-Park-GettyImages-949528764.jpg" />
                         <Card.Body>
                             <Card.Text>
-                                Visit our Mountains Page to explore the details of 48 different mountains
+                                Visit our Mountains Page to explore the details of 48 different mountains.
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="d-flex justify-content-center">
                             <LinkContainer to="/mountains">
-                                <Button variant="primary">
+                                <Button variant="secondary">
                                     Mountains Information
                                 </Button>
                             </LinkContainer>
