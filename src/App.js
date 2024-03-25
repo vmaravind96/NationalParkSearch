25648492dd0338eb22ed3eb6ca170c371daa568b
Eric Schwartz <erics273@gmail.com>
2024-03-25 00:02:58 -0400

    fixing repo file strucuture

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 // import our page components
 import Home from './pages/home/Home';
 import Navigation from './components/navigation/Navigation';
-import NationalParks from './components/national_parks/NationalParks';
-import Mountains from './components/mountains/Mountains';
+import NationalParks from './pages/national_parks/NationalParks';
+import Mountains from './pages/mountains/Mountains';
 import Footer from './components/footer/Footer';
 
 function App() {
